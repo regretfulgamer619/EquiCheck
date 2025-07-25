b68646b899d6eb2592c9b5cee904f830b5d9dfc8
Darpan Rathi <darpan.rathi.cd.phy24@itbhu.ac.in>
2025-07-25 16:35:55 +0530

    Update EquiCheck.py

--- EquiCheck.py
+++ EquiCheck.py
@@ -5,7 +5,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-image: url("https://raw.githubusercontent.com/regretfulgamer619/EquiCheck/main/bg.jpeg");
+        background-image: url("https://raw.githubusercontent.com/regretfulgamer619/EquiCheck/main/_.jpeg");
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
